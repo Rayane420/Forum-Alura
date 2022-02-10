7ba7593dd6b4185f055e09d958fe4340f9d34a2a
Rayane <rayanemaciel10@gmail.com>
2022-02-09 22:16:28 -0300

    Alteração na lista

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -3,6 +3,9 @@ package br.com.alura.forum.controller;
 import br.com.alura.forum.controller.dto.TopicoDto;
 import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.TopicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,12 +13,16 @@ import java.util.Arrays;
 import java.util.List;
 
 @RestController
+@RequestMapping("/topicos")
 public class TopicosController {
 
-    @RequestMapping("/topicos")
+    @Autowired
+    private TopicoRepository topicoRepository;
+
+    @GetMapping
     public List<TopicoDto> lista(){
-        Topico topico = new Topico("Dúvida", "Dúvida sobre como funciona o Hibernate", new Curso("Spring", "Programação"));
-        return TopicoDto.converter(Arrays.asList(topico, topico, topico));
+    List<Topico> topicos = topicoRepository.findAll();
+        return TopicoDto.converter(topicos);
     }
 
 
