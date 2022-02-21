605096125bc7a6ed602350528c53577a2b767c5c
Rayane <rayanemaciel10@gmail.com>
2022-02-21 20:39:37 -0300

    Adicionando cadastro de cursos

--- src/main/java/br/com/alura/forum/controller/CursoController.java
+++ src/main/java/br/com/alura/forum/controller/CursoController.java
@@ -1,13 +1,15 @@
 package br.com.alura.forum.controller;
 
 import br.com.alura.forum.controller.dto.CursoDto;
+import br.com.alura.forum.controller.form.CursoForm;
 import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.repository.CursoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -21,6 +23,18 @@ public class CursoController {
     public List<CursoDto>lista(){
         List<Curso> cursos = cursoRepository.findAll();
         return CursoDto.converter(cursos);
+    }
+
+    //TODO cadastrar cursos
+    @PostMapping
+    public ResponseEntity<CursoDto> cadastrar(@RequestBody CursoForm cursoForm, UriComponentsBuilder uriComponentsBuilder){
+        Curso curso = cursoForm.converter(cursoRepository);
+        cursoRepository.save(curso);
+
+        URI uri = uriComponentsBuilder.path("cursos/{id}").buildAndExpand(curso.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(new CursoDto(curso));
+
     }
 
 }
