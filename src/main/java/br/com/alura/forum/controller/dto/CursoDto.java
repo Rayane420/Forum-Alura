e544aa4e0ce5d51269d7659de6cd858d664c5648
Rayane <rayanemaciel10@gmail.com>
2022-02-22 23:29:44 -0300

    att cursoDto

--- src/main/java/br/com/alura/forum/controller/dto/CursoDto.java
+++ src/main/java/br/com/alura/forum/controller/dto/CursoDto.java
@@ -7,23 +7,15 @@ import java.util.stream.Collectors;
 
 public class CursoDto {
 
-    private Long id;
     private String nome;
     private String categoria;
 
     public CursoDto(Curso curso){
-        this.id = curso.getId();
+
         this.nome = curso.getNome();
         this.categoria = curso.getCategoria();
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 
     public String getNome() {
         return nome;
